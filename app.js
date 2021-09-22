79b81531d9164a5b4bdbaf9ffffdfe1b32f28294
Vi <engrycat@gmail.com>
2021-09-22 14:10:55 +0300

    commit

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ item.addEventListener('dragstart', dragstart)
 item.addEventListener('dragend', dragend)
 
 for (const placeholder of placeholders) {
-    placeholders.addEventListener('dragover')
-    placeholders.addEventListener('dragenter')
-    placeholders.addEventListener('dragleave')
-    placeholders.addEventListener('drop')
+    console.log(placeholder)
+    placeholder.addEventListener('dragover', dragover)
+    placeholder.addEventListener('dragenter', dragenter)
+    placeholder.addEventListener('dragleave', dragleave)
+    placeholder.addEventListener('drop', dragdrop)
 }
 
 function dragstart(event) {
@@ -19,3 +20,21 @@ function dragstart(event) {
 function dragend(event) {
     event.target.classList.remove('hold', 'hide')
 }
+
+function dragover(event) {
+    event.preventDefault()
+}
+
+function dragenter(event) {
+    event.target.classList.add('hovered')
+}
+
+function dragleave(event) {
+    event.target.classList.remove('hovered')
+}
+
+function dragdrop(event) {
+    event.target.classList.remove('hovered')
+    event.target.append(item)
+}
+
